f472ffb83d64dc44484e1fb32e816db94ffb9364
Joost van der Laan <joostvanderlaan@gmail.com>
2014-12-03 23:06:10 +0100

    images and readme

--- Gulpfile.js
+++ Gulpfile.js
@@ -641,8 +641,9 @@ var deploy = require('gulp-gh-pages');
 
 // deploy 'dist' folder to mwhelan.github.io github repo, master branch
 var options = { 
-    remoteUrl: 'https://github.com/joostvanderlaan/gulpax.git',
+ //   remoteUrl: 'https://github.com/joostvanderlaan/gulpax.git',
     branch: 'gh-pages'};
+
 gulp.task('deploy', function () {
     gulp.src('dist/**/*.*')
         .pipe(deploy(options));
